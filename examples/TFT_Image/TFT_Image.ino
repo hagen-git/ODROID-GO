814b0303d67e89fcd324cf89a3048be7d97fa9b5
Hagen Patzke <git@hpatzke.de>
2021-02-21 17:17:59 +0100

    simplify example

--- examples/TFT_Image/TFT_Image.ino
+++ examples/TFT_Image/TFT_Image.ino
@@ -1,11 +1,11 @@
 #include <odroid_go.h>
 #include <utility/bmp_map.h>
 
+// Show M5STACK logo from bmp_map.h (rgb565 format)
+
 void setup() {
   GO.begin();
-  GO.lcd.clearDisplay();
-  uint32_t p = (uint32_t)((void *)gImage_logoM5);
-  uint16_t *img = (uint16_t *)p;
+  uint16_t *img = (uint16_t *)((void *)gImage_logoM5);
   GO.lcd.pushRect(0,0,320,240,img);
 }
 
